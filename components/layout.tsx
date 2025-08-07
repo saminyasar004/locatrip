c260386c68b76ede64f53d8070b7c7e06774acc5
Samin Yasar <yasarsamin57@gmail.com>
2025-08-07 17:36:24 +0600

    dev: tab item url added

---
+++ components/layout.tsx
@@ -0,0 +1,71 @@
+import { useRouter } from 'expo-router';
+import { Briefcase, House, ThumbsUp, UserRound } from 'lucide-react-native';
+import { useState } from 'react';
+import { SafeAreaView, ScrollView, Text, TouchableHighlight, View } from 'react-native';
+import { cn } from 'utils';
+
+export default function Layout({ children }: { children?: React.ReactNode }) {
+  const router = useRouter();
+
+  const [activeTab, setActiveTab] = useState<'Home' | 'My Plan' | 'Local Recommend' | 'Profile'>(
+    'Home'
+  );
+  const tabs = [
+    {
+      name: 'Home',
+      icon: <House size={20} color={activeTab === 'Home' ? '#F86241' : '#63707C'} />,
+      url: '/home',
+    },
+    {
+      name: 'My Plan',
+      icon: <Briefcase size={20} color={activeTab === 'My Plan' ? '#F86241' : '#63707C'} />,
+      url: '/personalize/step1',
+    },
+    {
+      name: 'Local Recommend',
+      icon: <ThumbsUp size={20} color={activeTab === 'Local Recommend' ? '#F86241' : '#63707C'} />,
+      url: '/local-recommend',
+    },
+    {
+      name: 'Profile',
+      icon: <UserRound size={20} color={activeTab === 'Profile' ? '#F86241' : '#63707C'} />,
+      url: '/auth/login',
+    },
+  ];
+
+  return (
+    <SafeAreaView className="flex-1 bg-background">
+      <ScrollView className="flex-1" contentContainerStyle={{ paddingBottom: 20 }}>
+        {children}
+      </ScrollView>
+      <View className="fixed bottom-0 left-0 right-0 flex h-auto min-h-20 w-full flex-row items-center justify-around gap-4 border-t border-dark-gray/10 bg-white py-3">
+        {tabs.map((tab, index) => (
+          <TouchableHighlight
+            key={index}
+            onPress={() => {
+              setActiveTab(tab.name as any);
+              router.push(tab.url);
+            }}
+            underlayColor={'transparent'}>
+            <View className="flex flex-col items-center gap-1">
+              <View
+                className={cn(
+                  'flex h-14 w-14 items-center justify-center overflow-hidden rounded-full p-2',
+                  activeTab === tab.name ? 'bg-primary/20' : 'bg-white'
+                )}>
+                {tab.icon}
+              </View>
+              <Text
+                className={cn(
+                  'text-base font-medium',
+                  activeTab === tab.name ? 'text-primary' : 'text-[#63707C]'
+                )}>
+                {tab.name}
+              </Text>
+            </View>
+          </TouchableHighlight>
+        ))}
+      </View>
+    </SafeAreaView>
+  );
+}
