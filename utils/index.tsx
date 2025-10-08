848747897cde57d88cf38f37871f2e965f6a596c
Samin Yasar <yasarsamin57@gmail.com>
2025-10-08 18:06:18 +0600

    dev: localstorage lib functions added

--- utils/index.tsx
+++ utils/index.tsx
@@ -1,6 +1,33 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const setLocalStorageItem = async (key: string, value: string) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+  } catch (error) {
+    console.log('Error setting item to local storage', error);
+  }
+};
+
+export const getLocalStorageItem = async (key: string) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return value;
+  } catch (error) {
+    console.error('Error getting item:', error);
+    return null;
+  }
+};
+
+export const removeLocalStorageItem = async (key: string) => {
+  try {
+    await AsyncStorage.removeItem(key);
+  } catch (error) {
+    console.error('Error removing item:', error);
+  }
+};
