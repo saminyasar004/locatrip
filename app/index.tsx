ae7db01289ff8f77ddebe069e6144b0da3f8e279
Samin Yasar <yasarsamin57@gmail.com>
2025-10-09 16:37:37 +0600

    fix: onboarding page linked on the app first bootstrap

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,22 @@ import SVGImg from 'assets/adaptive-icon.svg';
 import { useRouter } from 'expo-router';
 import { useEffect } from 'react';
 import { SafeAreaView, View } from 'react-native';
+import useAuthStore from './store/authStore';
 
 export default function App() {
+  const { user, isAuthenticated, accessToken, isLoading } = useAuthStore();
+
   const router = useRouter();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      // router.replace('/onboarding');
+      if (!user || !isAuthenticated || !accessToken) {
+        if (!isLoading) {
+          router.replace('/onboarding');
+        }
+        return;
+      }
+
       router.replace('/home');
     }, 3000);
     return () => clearTimeout(timer);
