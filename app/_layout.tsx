2bd2610f746cd71b8dbf0e66c1d40f80f49d1d45
Samin Yasar <yasarsamin57@gmail.com>
2025-10-16 23:02:34 +0600

    fix: app root status bar fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { Stack } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ToastManager from 'toastify-react-native';
+import { StatusBar, useColorScheme } from 'react-native';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,9 +17,13 @@ const queryClient = new QueryClient({
 });
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const isDark = colorScheme === 'dark';
+
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar translucent={true} backgroundColor={isDark ? 'transparent' : '#000'} />
         <Stack
           screenOptions={{
             headerShown: false,
