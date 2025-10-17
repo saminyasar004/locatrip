d9e5ee514c9cb44b278499c89034f36d48ca8b76
Samin Yasar <yasarsamin57@gmail.com>
2025-10-17 13:37:37 +0600

    dev: bottom tav navigation styling updated

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,41 +1,58 @@
+import NoRippleTabButton from 'components/no-rippler-pressable';
 import { Tabs } from 'expo-router';
 import { Briefcase, House, ThumbsUp, UserRound } from 'lucide-react-native';
 
 export default function Layout() {
-    return (
-        <Tabs screenOptions={{ tabBarActiveTintColor: '#F86241', tabBarInactiveTintColor: '#63707C' }}>
-            <Tabs.Screen
-                name="home"
-                options={{
-                    headerShown: false,
-                    title: 'Home',
-                    tabBarIcon: ({ color, size }) => <House size={20} color={color} />,
-                }}
-            />
-            <Tabs.Screen
-                name="my-plan"
-                options={{
-                    headerShown: false,
-                    title: 'My Plan',
-                    tabBarIcon: ({ color, size }) => <Briefcase size={20} color={color} />,
-                }}
-            />
-            <Tabs.Screen
-                name="local-recommend"
-                options={{
-                    headerShown: false,
-                    title: 'Local Recommend',
-                    tabBarIcon: ({ color, size }) => <ThumbsUp size={20} color={color} />,
-                }}
-            />
-            <Tabs.Screen
-                name="profile"
-                options={{
-                    headerShown: false,
-                    title: 'Profile',
-                    tabBarIcon: ({ color, size }) => <UserRound size={20} color={color} />,
-                }}
-            />
-        </Tabs>
-    );
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          height: 60,
+          paddingTop: 0,
+          paddingBottom: 0,
+          marginBottom: 0,
+          marginTop: 0,
+          backgroundColor: '#fff',
+          borderTopWidth: 1,
+          borderTopColor: '#F2F2F2',
+        },
+        tabBarActiveTintColor: '#F86241',
+        tabBarInactiveTintColor: '#63707C',
+        tabBarButton: (props) => <NoRippleTabButton {...props} />,
+      }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerShown: false,
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => <House size={20} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="my-plan"
+        options={{
+          headerShown: false,
+          title: 'My Plan',
+          tabBarIcon: ({ color, size }) => <Briefcase size={20} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="local-recommend"
+        options={{
+          headerShown: false,
+          title: 'Local Recommend',
+          tabBarIcon: ({ color, size }) => <ThumbsUp size={20} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerShown: false,
+          title: 'Profile',
+          tabBarIcon: ({ color, size }) => <UserRound size={20} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
