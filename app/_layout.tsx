75b85db1a968c7161a173d71ab3cde60e49eb782
Samin Yasar <yasarsamin57@gmail.com>
2025-11-01 14:24:18 +0600

    fix: root layout status bar style fix and safeareaview fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,38 @@
 import 'nativewind';
 import './global.css';
 import { Stack } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ToastManager from 'toastify-react-native';
-import { StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaView, StatusBar, useColorScheme } from 'react-native';
 import { useEffect } from 'react';
 import useAuthStore from './store/authStore';
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 5 * 60 * 1000, // 5 min cache
-            retry: 3,
-            refetchOnWindowFocus: false, // RN doesn't have window focus
-        },
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 min cache
+      retry: 3,
+      refetchOnWindowFocus: false, // RN doesn't have window focus
     },
+  },
 });
 
 export default function RootLayout() {
-    const colorScheme = useColorScheme();
-    const isDark = colorScheme === 'dark';
-    return (
-        <QueryClientProvider client={queryClient}>
-            <SafeAreaView style={{ flex: 1 }}>
-                <StatusBar translucent={true} backgroundColor={isDark ? 'transparent' : '#000'} />
-                <Stack
-                    screenOptions={{
-                        headerShown: false,
-                    }}>
-                    {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
-                    <Stack.Screen name="index" options={{ headerShown: false }} />
-                </Stack>
-                <ToastManager />
-            </SafeAreaView>
-        </QueryClientProvider>
-    );
+  const colorScheme = useColorScheme();
+  const isDark = colorScheme === 'dark';
+  return (
+    <QueryClientProvider client={queryClient}>
+      <StatusBar translucent={true} backgroundColor={'#000'} barStyle="dark-content" hidden />
+      <SafeAreaView className="h-full w-full flex-1">
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}>
+          {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+        <ToastManager />
+      </SafeAreaView>
+    </QueryClientProvider>
+  );
 }
