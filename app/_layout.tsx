7f2556e4fd4b06c616e74d1ce9d6ecb4b5b4038b
Samin Yasar <yasarsamin57@gmail.com>
2025-11-01 17:52:40 +0600

    fix: app root layout safeareaview fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,10 @@
-import 'nativewind';
-import './global.css';
+import { QueryClient } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import 'nativewind';
+import { StatusBar, useColorScheme } from 'react-native';
+import { Host } from 'react-native-portalize';
 import ToastManager from 'toastify-react-native';
-import { SafeAreaView, StatusBar, useColorScheme } from 'react-native';
-import { useEffect } from 'react';
-import useAuthStore from './store/authStore';
+import './global.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,18 +20,31 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const isDark = colorScheme === 'dark';
   return (
-    <QueryClientProvider client={queryClient}>
-      <StatusBar translucent={true} backgroundColor={'#000'} barStyle="dark-content" hidden />
-      <SafeAreaView className="h-full w-full flex-1">
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}>
-          {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-        </Stack>
-        <ToastManager />
-      </SafeAreaView>
-    </QueryClientProvider>
+    // <QueryClientProvider client={queryClient}>
+    //   <StatusBar translucent={true} backgroundColor={'#000'} barStyle="dark-content" hidden />
+    //   <SafeAreaView className="h-full w-full flex-1">
+    //     <Stack
+    //       screenOptions={{
+    //         headerShown: false,
+    //       }}>
+    //       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    //       <Stack.Screen name="index" options={{ headerShown: false }} />
+    //     </Stack>
+    //     <ToastManager />
+    //   </SafeAreaView>
+    // </QueryClientProvider>
+
+    <Host>
+      <StatusBar backgroundColor="#fff" barStyle="dark-content" />
+
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+      <ToastManager />
+    </Host>
   );
 }
