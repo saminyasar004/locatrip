2be9333622dc0f1000c1a7a39957db0bcd6d743d
Samin Yasar <yasarsamin57@gmail.com>
2025-11-01 17:56:14 +0600

    dev: missed layout added on the onboarding and auth page

---
+++ app/auth/_layout.tsx
@@ -0,0 +1,27 @@
+import { Stack } from 'expo-router';
+import { StatusBar, useColorScheme } from 'react-native';
+
+export default function AuthLayout() {
+  const colorScheme = useColorScheme();
+  const isDark = colorScheme === 'dark';
+
+  return (
+    <>
+      <StatusBar
+        translucent={true}
+        backgroundColor={isDark ? 'transparent' : '#000'}
+        barStyle="dark-content"
+      />
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </>
+  );
+}
