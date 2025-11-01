5e0f4dc1ec5f7fe53e8f2ea41714a5fa7bdb7ecd
Samin Yasar <yasarsamin57@gmail.com>
2025-11-01 17:58:07 +0600

    dev: personalize preference item fetching functionality added

--- app/store/personalizeStore.tsx
+++ app/store/personalizeStore.tsx
@@ -3,25 +3,58 @@ import { baseURL } from 'config';
 import { create } from 'zustand';
 import useAuthStore from './authStore';
 
-interface PreferenceIdType {
+export interface PreferenceProps {
   id: number;
+  name: string;
+  slug: string;
+  created_at: Date;
+  updated_at: Date;
 }
 
 interface PersonalizeState {
+  preferenceList: PreferenceProps[];
   personalizeItems: number[];
   personalizeItemsNames: string[];
+  fetchAllPreferences: () => Promise<void>;
   isLoading: boolean;
   error: string | null;
-  createPreference: (preferenceId: PreferenceIdType[]) => Promise<void>;
+  createPreference: (preferenceId: number[]) => Promise<void>;
 }
 
 const usePersonalizeStore = create<PersonalizeState>((set, get) => ({
+  preferenceList: [],
   personalizeItems: [],
   isLoading: false,
   error: null,
   personalizeItemsNames: [],
 
-  createPreference: async (preferenceId: PreferenceIdType[]) => {
+  fetchAllPreferences: async () => {
+    const accessToken = useAuthStore.getState().accessToken;
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.get(`${baseURL}/api/personalize/interests/`, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+
+      console.log('Preference response:', response.data);
+
+      // Adjust this according to your backend's response structure
+      console.log('Preference names:', response.data);
+      // Save token securely
+      set({
+        preferenceList: response.data,
+        isLoading: false,
+        error: null,
+      });
+    } catch (error: any) {
+      set({
+        error: error?.response?.data?.message || error.message || 'Login failed',
+        isLoading: false,
+      });
+    }
+  },
+
+  createPreference: async (preferenceId: number[]) => {
     const accessToken = useAuthStore.getState().accessToken;
     set({ isLoading: true, error: null });
     try {
