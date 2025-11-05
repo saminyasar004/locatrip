8443790ae9012dd59e09eb86578587d3679592ab
Samin Yasar <yasarsamin57@gmail.com>
2025-11-05 11:41:37 +0600

    fix: app directory cleaned. unnecessary and non-page file removed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,36 +4,12 @@ import 'nativewind';
 import { StatusBar, useColorScheme } from 'react-native';
 import { Host } from 'react-native-portalize';
 import ToastManager from 'toastify-react-native';
-import './global.css';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 5 * 60 * 1000, // 5 min cache
-      retry: 3,
-      refetchOnWindowFocus: false, // RN doesn't have window focus
-    },
-  },
-});
+import '../global.css';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const isDark = colorScheme === 'dark';
   return (
-    // <QueryClientProvider client={queryClient}>
-    //   <StatusBar translucent={true} backgroundColor={'#000'} barStyle="dark-content" hidden />
-    //   <SafeAreaView className="h-full w-full flex-1">
-    //     <Stack
-    //       screenOptions={{
-    //         headerShown: false,
-    //       }}>
-    //       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    //       <Stack.Screen name="index" options={{ headerShown: false }} />
-    //     </Stack>
-    //     <ToastManager />
-    //   </SafeAreaView>
-    // </QueryClientProvider>
-
     <Host>
       <StatusBar backgroundColor="#fff" barStyle="dark-content" />
 
