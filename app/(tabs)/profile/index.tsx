d7634b87c0f9a71dfcba181a9294bd34257edb89
Samin Yasar <yasarsamin57@gmail.com>
2025-11-13 12:17:24 +0600

    fix: profile page subscription route duplication removed

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -110,20 +110,6 @@ export default function Index() {
 
           <View className="flex w-full flex-row items-center justify-between rounded-lg border-2 border-primary/40 bg-white px-3 py-4">
             <View className="flex flex-row items-center gap-4">
-              <Wallet size={24} color={'#F86241'} />
-
-              <Text className="font-medium">Subscriptions</Text>
-            </View>
-
-            <TouchableHighlight
-              onPress={() => router.push('/my-plan/reviews')}
-              underlayColor={'transparent'}>
-              <ChevronRight size={25} color={'#F86241'} />
-            </TouchableHighlight>
-          </View>
-
-          <View className="flex w-full flex-row items-center justify-between rounded-lg border-2 border-primary/40 bg-white px-3 py-4">
-            <View className="flex flex-row items-center gap-4">
               <Heart size={24} color={'#F86241'} />
 
               <Text className="font-medium">Bookmark</Text>
