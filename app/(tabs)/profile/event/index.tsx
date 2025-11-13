e22e4bfb5029cec0abbf61bf9773b85b529ff15c
Samin Yasar <yasarsamin57@gmail.com>
2025-11-13 10:13:56 +0600

    dev: create event page routing linked

--- app/(tabs)/profile/event/index.tsx
+++ app/(tabs)/profile/event/index.tsx
@@ -33,7 +33,7 @@ export default function Index() {
 
         <View className="flex w-full items-center justify-center py-5">
           <TouchableHighlight
-            onPress={() => router.push('/create-event')}
+            onPress={() => router.push('/profile/event/create-event')}
             className="flex w-full items-center justify-center rounded-full border-2 border-primary bg-background px-4 py-4 shadow-sm"
             underlayColor="transparent">
             <View className="flex flex-row items-center gap-2">
