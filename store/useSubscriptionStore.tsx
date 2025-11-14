ab640ddcb11b99aa3e0c79fee51cd374bdee3262
Samin Yasar <yasarsamin57@gmail.com>
2025-11-14 16:28:54 +0600

    dev: subscription store hook added

---
+++ store/useSubscriptionStore.tsx
@@ -0,0 +1,63 @@
+import { baseURL } from '@/config';
+import { create } from 'zustand';
+import useAuthStore from './authStore';
+import axios, { AxiosResponse } from 'axios';
+
+export interface SubscriptionItemProps {
+  id: number;
+  plan_name: string;
+  duration: number;
+  price: number;
+  itinerary_limit: number;
+  feature_1: string;
+  feature_2: string;
+  feature_3: string;
+  feature_4: string;
+  feature_5: string;
+  feature_6: string;
+  feature_7: string;
+  feature_8: string;
+  feature_9: string;
+  feature_10: string;
+}
+
+export interface SubscriptionStoreState {
+  payment: SubscriptionItemProps[];
+  isLoading: boolean;
+  error: string | null;
+  fetchAllSubscription: () => Promise<AxiosResponse<any, any, {}> | undefined>;
+}
+
+const useSubscriptionStore = create<SubscriptionStoreState>((set, get) => ({
+  payment: [],
+  isLoading: false,
+  error: null,
+
+  fetchAllSubscription: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      const accessToken = useAuthStore.getState().accessToken;
+      const response = await axios.get(`${baseURL}/api/payment/all_subscription_list/`, {
+        headers: { Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json' },
+      });
+
+      if (response.status === 200) {
+        set({ payment: response.data.payment, isLoading: false, error: null });
+      } else {
+        set({
+          payment: [],
+          isLoading: false,
+          error: response.data.message || response.data.detail || response.data.error,
+        });
+      }
+      return response;
+    } catch (error: any) {
+      set({
+        error: error?.response?.data?.message || error.message || 'Login failed',
+        isLoading: false,
+      });
+    }
+  },
+}));
+
+export default useSubscriptionStore;
