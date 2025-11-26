8e05d588fa6965b7b3cfd52c91ba4284fb4cd69a
Samin Yasar <yasarsamin57@gmail.com>
2025-11-26 10:17:46 +0600

    feat: add personalize flow navigation layout with steps

---
+++ app/personalize/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from 'expo-router';
+
+export default function PersonalizeLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="step1" />
+      <Stack.Screen name="step2" />
+      <Stack.Screen name="step3" />
+      <Stack.Screen name="step4" />
+      <Stack.Screen name="step5" />
+    </Stack>
+  );
+}
