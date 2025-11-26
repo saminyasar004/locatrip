44c8a5845f7dbd132b29a6b7334a8a0648848b99
Samin Yasar <yasarsamin57@gmail.com>
2025-11-26 17:58:54 +0600

    feat: Remove `react-native-reanimated` dependency, wrap subscription view with `Layout`, and refine registration navigation delay.

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,6 @@ module.exports = function (api) {
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-    plugins: ['react-native-worklets/plugin', 'react-native-reanimated/plugin'],
+    plugins: ['react-native-worklets/plugin'],
   };
 };
