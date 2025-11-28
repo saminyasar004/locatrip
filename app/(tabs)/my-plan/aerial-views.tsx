2f4a7a19e59c838b76c572b8793c3a27e0556191
Samin Yasar <yasarsamin57@gmail.com>
2025-11-28 16:35:36 +0600

    feat: Implement dynamic data fetching, search, and display for nearest places, replacing static content.

--- app/(tabs)/my-plan/aerial-views.tsx
+++ app/(tabs)/my-plan/aerial-views.tsx
@@ -1,15 +1,20 @@
 import Layout from 'components/layout';
-import { useRouter } from 'expo-router';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import { ArrowLeft } from 'lucide-react-native';
 import { Image, Text, TouchableHighlight, View } from 'react-native';
 
 export default function Index() {
   const router = useRouter();
+  const params = useLocalSearchParams();
+  const { photos: photosParam } = params;
+
+  // Parse photos from params
+  const photos = photosParam ? JSON.parse(photosParam as string) : [];
 
   return (
     <Layout>
       <View className="row flex h-auto min-h-full w-full flex-1 flex-col items-start">
-        <View className="flex w-full flex-row items-center gap-3 bg-white">
+        <View className="flex w-full flex-row items-center gap-3 bg-white pt-4">
           <TouchableHighlight onPress={() => router.back()} underlayColor={'transparent'}>
             <ArrowLeft size={24} color={'#63707C'} />
           </TouchableHighlight>
@@ -17,89 +22,23 @@ export default function Index() {
           <Text className="text-lg font-semibold text-[#313131]">Aerial Views</Text>
         </View>
 
-        <View className="flex w-full flex-row gap-5 py-3">
-          <Image
-            source={require('assets/aerial-views-1.jpg')}
-            className="h-52 w-[48%] rounded-2xl"
-            resizeMode="cover"
-          />
-
-          <Image
-            source={require('assets/aerial-views-2.jpg')}
-            className="h-52 w-[48%] rounded-2xl"
-            resizeMode="cover"
-          />
-        </View>
-
-        <View className="flex w-full flex-row gap-5 py-3">
-          <Image
-            source={require('assets/art-1.jpg')}
-            className="h-52 w-[48%] rounded-2xl"
-            resizeMode="cover"
-          />
-
-          <Image
-            source={require('assets/art-2.jpg')}
-            className="h-52 w-[48%] rounded-2xl"
-            resizeMode="cover"
-          />
-        </View>
-
-        <View className="flex w-full flex-row gap-5 py-3">
-          <Image
-            source={require('assets/landscape.jpg')}
-            className="h-52 w-[48%] rounded-2xl"
-            resizeMode="cover"
-          />
-
-          <Image
-            source={require('assets/dinner.jpg')}
-            className="h-52 w-[48%] rounded-2xl"
-            resizeMode="cover"
-          />
-        </View>
-
-        <View className="flex w-full flex-row gap-5 py-3">
-          <Image
-            source={require('assets/event-1.jpg')}
-            className="h-52 w-[48%] rounded-2xl"
-            resizeMode="cover"
-          />
-
-          <Image
-            source={require('assets/event-2.jpg')}
-            className="h-52 w-[48%] rounded-2xl"
-            resizeMode="cover"
-          />
-        </View>
-
-        <View className="flex w-full flex-row gap-5 py-3">
-          <Image
-            source={require('assets/event-3.jpg')}
-            className="h-52 w-[48%] rounded-2xl"
-            resizeMode="cover"
-          />
-
-          <Image
-            source={require('assets/event-4.jpg')}
-            className="h-52 w-[48%] rounded-2xl"
-            resizeMode="cover"
-          />
-        </View>
-
-        <View className="flex w-full flex-row gap-5 py-3">
-          <Image
-            source={require('assets/event-5.jpg')}
-            className="h-52 w-[48%] rounded-2xl"
-            resizeMode="cover"
-          />
-
-          <Image
-            source={require('assets/event-6.jpg')}
-            className="h-52 w-[48%] rounded-2xl"
-            resizeMode="cover"
-          />
-        </View>
+        {photos.length > 0 ? (
+          <View className="flex w-full flex-row flex-wrap justify-between py-3">
+            {photos.map((photo: string, index: number) => (
+              <View key={index} className="mb-[10px] w-[48%]">
+                <Image
+                  source={{ uri: photo }}
+                  className="h-52 w-full rounded-2xl"
+                  resizeMode="cover"
+                />
+              </View>
+            ))}
+          </View>
+        ) : (
+          <View className="flex-1 items-center justify-center py-10">
+            <Text className="text-gray-500">No photos available.</Text>
+          </View>
+        )}
       </View>
     </Layout>
   );
